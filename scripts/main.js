b82441f3a05489950f3bf6ef37f8fb421e79c5d3
Eduard Maksakov <maksakoved09@gmail.com>
2025-07-18 11:57:32 +0700

    Add 'FutureTech' files

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+import Header from "./Header.js";
+import TabsCollection from "./Tabs.js";
+import videoPlayerCollection from "./VideoPlayer.js";
+import ExpandableContentCollection from "./ExpandableContent.js"
+import InputMaskCollection from "./InputMask.js";
+import SelectCollection from "./Select.js";
+
+new Header();
+new TabsCollection();
+new videoPlayerCollection();
+new ExpandableContentCollection();
+new InputMaskCollection();
+new SelectCollection();
